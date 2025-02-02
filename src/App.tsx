749d2dd1004fd9786168546cf8f270acad89c1dd
Amin Meraji <58762298+a-meraji@users.noreply.github.com>
2025-02-02 16:40:54 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import Profile from "@/pages/Profile";
 import ReturnsExchanges from "@/pages/ReturnsExchanges";
 import ShippingPolicy from "@/pages/ShippingPolicy";
 import Admin from "@/pages/Admin";
-
+import CarouselsUI from "@pages/CarouselsUI";
 const queryClient = new QueryClient();
 
 function App() {
@@ -27,6 +27,7 @@ function App() {
           <Route element={<RootLayout />}>
             <Route path="/" element={<Index />} />
             <Route path="/watch" element={<Watch />} />
+            <Route path="/Carousels" element={<CarouselsUI />} />
             <Route path="/about" element={<About />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/cart" element={<Cart />} />
